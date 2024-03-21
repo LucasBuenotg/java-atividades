8aef3e5cce8f9711027d49554417b9998c5ed3ac
LucasBuenotg <128059480+LucasBuenotg@users.noreply.github.com>
2024-03-21 10:11:25 -0300

    Add files via upload

---
+++ atividade Java/exercicio.js
@@ -0,0 +1,22 @@
+// Exercício 1: 
+// Variáveis e Tipos de Dados
+// Declare uma variável chamada idade e atribua a ela o valor de sua idade.
+// Declare uma variável chamada nome e atribua a ela o seu nome.
+// Declare uma constante chamada PI e atribua o valor de π (pi) a ela.
+// Crie uma variável chamada estaChovendo e atribua a ela o valor booleano 
+// verdadeiro se estiver chovendo e falso caso contrário.
+
+let idade = 19;
+
+let nome = "Lucas Bueno";
+
+const PI = '3,14';
+
+let chuva = 1;
+if (idade != 1) {
+    console.log("está chovendo.");
+} else {
+    console.log("não está chovendo.");
+}
+
+
